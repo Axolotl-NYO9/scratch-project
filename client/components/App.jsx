0a8b319f2cf34b65ebe4040bf3b9dca0dc4a8b64
Brandon <brandon5.9@hotmail.com>
2024-01-08 14:15:44 -0500

    Working on the emotions file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,26 @@
 //import react and react-dom
 import React from "react";
 import {render} from "react-dom";
-import SignUpButton from "./SignUp.jsx"
+import EmotionsButtons from "./EmotionsButtons.jsx"
 
 
 //continue here.. 
 const App = () => {
     // return <h1>hello world</h1>
-    return (
-        <div>
-            <h1>hello world</h1>
-            <SignUpButton />
-        </div>
-    )
+
+    return <BoredButton/>
+//     return (
+//         <div className="app">
+//             <h1>Challenge Booster</h1>
+//             <div className="buttons">
+//                 <BoredButton />
+//                 {/* <SignUpButton /> */}
+//                 {/* <SignUpButton /> */}
+//                 {/* <SignUpButton /> */}
+//                 {/* <SignUpButton /> */}
+//             </div>
+//         </div>
+//     )
 }
 
 export default App;
---
+++ client/components/EmotionsButtons.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../style/style.css"
+
+// Remember that this button will redirect to localhost8080/signUp
+function BoredButton() {
+
+        return <h1>hello world</h1>
+
+//     return (
+//      <button 
+//      className='boredButton'
+//      // When user clicks this, give the user the html page (On click will do a get request to the server)
+//      onClick={() => alert('Going to sign up for Energy Life Pakage! (Page Loading...)')}
+//      >
+//              : |
+//      </button>
+//  )
+};
+
+export default BoredButton;
